932115e1b9440194d575e2a816b07ef7aa37c82c
RITIK SHARMA <120105490+ritik564@users.noreply.github.com>
2024-07-27 10:33:10 +0530

    Time: 28 ms (56.2%), Space: 15.6 MB (90.04%) - LeetHub

---
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n = num.size();
+        for(int i=n-1; i>=0 ; i--)
+        {
+            if(num[i] % 2 != 0)
+            {
+                return num;
+            }
+            else
+            {
+                num.erase(i,1);
+            }
+        }
+        return "";
+    }
+};
